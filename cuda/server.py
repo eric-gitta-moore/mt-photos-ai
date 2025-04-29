65b40aa78c9ea3e9536fc83113ba7d0f1e59d0c2
Eric Moore <EricGittaMoore@duck.com>
2025-04-29 11:11:18 +0800

    feat: 更新Dockerfile以注释掉模型复制命令，并在server.py中使用f-string格式化HTML内容，添加推理设备信息。

--- cuda/server.py
+++ cuda/server.py
@@ -141,16 +141,17 @@ def convert_rapidocr_to_json(rapidocr_output):
 
 @app.get("/", response_class=HTMLResponse)
 async def top_info():
-    html_content = """<!DOCTYPE html>
+    html_content = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>MT Photos AI Server</title>
-    <style>p{text-align: center;}</style>
+    <style>p{{text-align: center;}}</style>
 </head>
 <body>
 <p style="font-weight: 600;">MT Photos智能识别服务</p>
 <p>服务状态： 运行中</p>
+<p>推理设备： {device}</p>
 <p>使用方法： <a href="https://mtmt.tech/docs/advanced/ocr_api">https://mtmt.tech/docs/advanced/ocr_api</a></p>
 </body>
 </html>"""
